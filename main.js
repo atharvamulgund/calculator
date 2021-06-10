eafafdd7f06d167d3cc004fd9184f6525aa8e8fe
Atharva Mulgund <63964451+atharvamulgund@users.noreply.github.com>
2021-06-10 07:46:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+let screen = document.getElementById('screen');
+let buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        let buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'Clear') {
+            screenValue = "";
+            //screen.value is js function
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
+
